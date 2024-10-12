556d0f0d5e76080d6cf312d30c3a5e97467c92b2
yikang wu <2317775630@qq.com>
2024-10-12 10:01:42 +0800

    使用Prism架构搭建一个WPF程序，并在一个Region内添加一个View（任意）

---
+++ task37/Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Prism.Regions;
+using System.Windows;
+
+namespace task37.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(IRegionManager regionManager)
+        {
+            InitializeComponent();
+            regionManager.RegisterViewWithRegion("ContentRegion", typeof(home));
+        }
+    }
+}
---
+++ task37/Views/home.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task37.Views
+{
+    public partial class home
+    {
+        public home()
+        {
+            InitializeComponent();
+           
+        }
+
+
+
+    }
+}
